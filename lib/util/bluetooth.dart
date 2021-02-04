94868674665bf39b00898838ebf462f1d37791a2
MohamadEslim <mohammadeslim@gmail.com>
2021-02-04 09:57:09 +0200

    barcode fix

--- lib/util/bluetooth.dart
+++ lib/util/bluetooth.dart
@@ -422,7 +422,7 @@ class _MyAppState extends State<Bluetooth> {
                 0);
             getIt<Auth>()
                 .bluetooth
-                .printCustom("    ${transaction.details[i].barcode}", 1, 0);
+                .printCustom("    ${element.details[i].barcode}", 1, 0);
           }
           orderAmount += element.amount;
           discount += double.parse(element.discount ?? "0");
@@ -444,7 +444,7 @@ class _MyAppState extends State<Bluetooth> {
                 0);
             getIt<Auth>()
                 .bluetooth
-                .printCustom("    ${transaction.details[i].barcode}", 1, 0);
+                .printCustom("    ${element.details[i].barcode}", 1, 0);
           }
           returnAmount += element.amount;
           getIt<Auth>().bluetooth.printNewLine();
